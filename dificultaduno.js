2a45beb470e696e9bfbbbdd2da8a2297db127f6a
vinculos-ludicos <117417875+vinculos-ludicos@users.noreply.github.com>
2022-11-03 22:42:07 -0300

    1er muestra

---
+++ dificultaduno.js
@@ -0,0 +1,19 @@
+function shuffle()
+{
+    const img=document.getElementById("img");
+    let randomNumber=Math.floor(Math.random() * 6) +1 ;
+    img.setAttribute("src" , `${randomNumber}.png`);
+    
+}
+
+function anim()
+{
+    setTimeout(shuffle,2000);
+    const img = document.getElementById("img");
+    img.setAttribute("src","rollingdice.gif");
+    var puntero=document.getElementById('siguiente');
+}
+function prenda(){
+  window.location.href="./prendauno.html";
+  }
+
---
+++ index.js
@@ -0,0 +1,9 @@
+function tirardadosuno(){
+        window.location.href="./dificultaduno.html";
+        }
+function tirardadosdos(){
+                window.location.href="./dificultaddos.html";
+                }
+function tirardadostres(){
+            window.location.href="./dificultadtres.html";
+            }        
