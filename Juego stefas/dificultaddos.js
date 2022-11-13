7b98c4d7fc7b8c1086fc24c7162cb0ebe2029abc
vinculos-ludicos <117417875+vinculos-ludicos@users.noreply.github.com>
2022-11-13 20:08:49 -0300

    2do commit

---
+++ Juego stefas/dificultaddos.js
@@ -0,0 +1,32 @@
+function shuffle()
+{
+    const img=document.getElementById("img");
+    let randomNumber=Math.floor(Math.random() * 6) +1 ;
+    img.setAttribute("src" , `${randomNumber}.png`);
+    
+}
+
+
+const cargarSonido = function (fuente) {
+    const sonido = document.createElement("audio");
+    sonido.src = fuente;
+    sonido.setAttribute("preload", "auto");
+    sonido.setAttribute("controls", "none");
+    sonido.style.display = "none"; // <-- oculto
+    document.body.appendChild(sonido);
+    return sonido;
+};
+
+function anim()
+{
+    setTimeout(shuffle,2500);
+    const img = document.getElementById("img");
+    img.setAttribute("src","rollingdice.gif");
+    var puntero=document.getElementById('siguiente');
+
+    const sonido = cargarSonido("./dado.mp3");
+    sonido.play();}
+function prenda(){
+  window.location.href="./prendados.html";
+  }
+
---
+++ Juego stefas/index.js
@@ -0,0 +1,6 @@
+function uno(){
+    window.location.href="./juegouno.html";
+    }
+function dos(){
+            window.location.href="./juegodos.html";
+            }
