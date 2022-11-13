f6a3069c839b41a2440529f77eab8a95cfab9399
vinculos-ludicos <117417875+vinculos-ludicos@users.noreply.github.com>
2022-11-13 20:10:04 -0300

    Carpeta mal agregada

--- Juego stefas/index.js
+++
@@ -1,6 +0,0 @@
-function uno(){
-    window.location.href="./juegouno.html";
-    }
-function dos(){
-            window.location.href="./juegodos.html";
-            }
--- Juego stefas/juegouno.js
+++
@@ -1,9 +0,0 @@
-function tirardadosuno(){
-        window.location.href="./dificultaduno.html";
-        }
-function tirardadosdos(){
-                window.location.href="./dificultaddos.html";
-                }
-function tirardadostres(){
-            window.location.href="./dificultadtres.html";
-            }        
